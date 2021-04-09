eeff7f02d1349d3854e63e66508d823457e9c8c9
Rick <63522977+Rybinsmc@users.noreply.github.com>
2021-04-08 21:05:46 -0500

    Add files via upload

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,29 @@
+import random
+
+random_number = random.randint(1,10)
+
+player_guess = input("Guess the random number! (1-10): ")
+player_guess = int(player_guess)
+
+while True:
+	if player_guess < random_number:
+		print("Too low!")
+		player_guess = input("Guess the random number! (1-10): ")
+		player_guess = int(player_guess)
+	elif player_guess > random_number:
+		print("Too high!")
+		player_guess = input("Guess the random number! (1-10): ")
+		player_guess = int(player_guess)
+	elif player_guess == random_number:
+		print("You win!")
+		play_again = input("Would you like to play again? (Y/N): ").upper()
+		if play_again == "Y":
+			player_guess = input("Guess the random number! (1-10): ")
+			player_guess = int(player_guess)
+		else:
+			print("Thank you for playing!")
+			break
+
+	else:
+		print("Please enter a valid input.")
+		break
