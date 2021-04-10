2a0bb403f893e81264ff9a07cdf1e9166cf0b76c
Rick <63522977+Rybinsmc@users.noreply.github.com>
2021-04-09 21:39:03 -0500

    Updated Looping
    The number stayed the same on play_again, fixed.

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -6,24 +6,25 @@ player_guess = input("Guess the random number! (1-10): ")
 player_guess = int(player_guess)
 
 while True:
-	if player_guess < random_number:
-		print("Too low!")
-		player_guess = input("Guess the random number! (1-10): ")
-		player_guess = int(player_guess)
-	elif player_guess > random_number:
-		print("Too high!")
-		player_guess = input("Guess the random number! (1-10): ")
-		player_guess = int(player_guess)
-	elif player_guess == random_number:
-		print("You win!")
-		play_again = input("Would you like to play again? (Y/N): ").upper()
-		if play_again == "Y":
-			player_guess = input("Guess the random number! (1-10): ")
-			player_guess = int(player_guess)
-		else:
-			print("Thank you for playing!")
-			break
-
-	else:
-		print("Please enter a valid input.")
-		break
+    if player_guess < random_number:
+        print("Too low!")
+        player_guess = input("Guess the random number! (1-10): ")
+        player_guess = int(player_guess)
+    elif player_guess > random_number:
+        print("Too high!")
+        player_guess = input("Guess the random number! (1-10): ")
+        player_guess = int(player_guess)
+    elif player_guess == random_number:
+        print("You won!")
+        play_again = input("Would you like to play again? (Y/N): ").upper()
+        if play_again == "Y":
+            random_number = random.randint(1, 10)
+            player_guess = input("Guess the random number! (1-10): ")
+            player_guess = int(player_guess)
+        else:
+            print("Thank you for playing!")
+            break
+    else:
+        print("Please enter a valid input.")
+        player_guess = input("Guess the random number! (1-10): ")
+        player_guess = int(player_guess)
